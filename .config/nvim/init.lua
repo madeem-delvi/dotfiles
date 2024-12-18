0d551212093f2ed19d7bf22c9d88c910269a009a
Francesca Pham <francescapham5055@gmail.com>
2024-12-17 20:26:30 -0500

    silly mistake, made plugin imports easier

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -18,20 +18,4 @@ vim.opt.rtp:prepend(lazypath)
 -- Personal settings not defined by plugins
 require("settings")
 
--- Usage: { import = "plugins.treesitter" },
-require("lazy").setup({
-  spec = {
-  { import = "plugins.vim-test" },
-  { import = "plugins.tmux-nav" },
-  { import = "plugins.completion" },
-  { import = "plugins.none-ls" },
-	{ import = "plugins.lsp-config" },
-	{ import = "plugins.indentation" },
-	{ import = "plugins.colorscheme" },
-	{ import = "plugins.treesitter" },
-	{ import = "plugins.telescope" },
-	{ import = "plugins.neotree" },
-	{ import = "plugins.lualine" },
-  },
-  checker = { enabled = true },
-})
+require("lazy").setup("plugins")
