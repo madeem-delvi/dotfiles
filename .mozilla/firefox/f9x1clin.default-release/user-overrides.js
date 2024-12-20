d3f337188290d42a3fa169ba3b9fe5aa51d285cc
Francesca Pham <francescapham5055@gmail.com>
2024-12-20 15:04:40 -0500

    added overrides file from Arkenfox configurations

---
+++ .mozilla/firefox/f9x1clin.default-release/user-overrides.js
@@ -0,0 +1,11 @@
+// Documentation: https://kb.mozillazine.org/About:config_entries
+// GUI: https://arkenfox.github.io/gui/
+
+// 0102: set startup page to resume session
+user_pref("browser.startup.page", 3);
+user_pref("browser.startup.homepage", "reddit.com");
+user_pref("privacy.clearOnShutdown.history", false); 
+user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false); 
+
+user_pref("geo.enabled", false);
+user_pref("browser.shell.checkDefaultBrowser", false);
