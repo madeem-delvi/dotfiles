60306e7bf220d08a0030285c519f462563b5363d
Francesca Pham <francescapham5055@gmail.com>
2024-12-22 14:23:02 -0500

    modified search behavior

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -1,3 +1,4 @@
+vim.opt.termguicolors = true
 -- Indentation Behavior
 vim.cmd("set autoindent")
 vim.cmd("set smartindent")
@@ -7,9 +8,14 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 
--- Leader key
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+-- Tab char & trailing whitespace
+vim.opt.list = true
+
+-- Search behavior
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
 -- Shows line numbers (relative and absolute)
 vim.opt.number = true
@@ -19,12 +25,20 @@ vim.o.statuscolumn = "%s %l %r"
 -- Force it to share clipboard with system
 vim.opt.clipboard = "unnamedplus"
 
+-- Disable org. status line (using lualine)
+vim.opt.showmode = false
+
+-- Key Remappings ---------------------------
+
 -- Window navigation
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
--- Disable org. status line (using lualine)
-vim.opt.showmode = false
+-- Leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
+local defaults = { noremap = true, silent = true }
+vim.keymap.set('i', '<C-H>', '<C-w>', defaults)
