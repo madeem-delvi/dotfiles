28f126c6a009cc23c61e2789b5790c5b9e0fc772
Francesca Pham <francescapham5055@gmail.com>
2025-01-03 17:29:01 -0500

    allowed history to be wiped on shutdown

--- .mozilla/firefox/f9x1clin.default-release/user-overrides.js
+++ .mozilla/firefox/f9x1clin.default-release/user-overrides.js
@@ -1,14 +1,15 @@
 // Documentation: https://kb.mozillazine.org/About:config_entries
 // GUI: https://arkenfox.github.io/gui/
 
+// 2830: Clear history
+user_pref("privacy.clearHistory.cookiesAndStorage", true);
+
 // 0102: set startup page to resume session
 user_pref("browser.startup.page", 3);
 user_pref("browser.startup.homepage", "reddit.com");
-user_pref("privacy.clearOnShutdown.history", false); 
-user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false); 
 
-user_pref("geo.enabled", false);
 user_pref("browser.shell.checkDefaultBrowser", false);
 
 // Get rid of default tab manager (using tree)
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true)
+
